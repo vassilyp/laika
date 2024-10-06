992a00c98454e6e21762712632e4881b1ccb91a8
tysonpbr <thetysonbrown@gmail.com>
2024-10-05 21:11:15 -0700

    updated button to only be hoverable once loaded

--- components/TitleOverlay.js
+++ components/TitleOverlay.js
@@ -27,10 +27,10 @@ export default function TitleOverlay({ startHandler }) {
 
       <div className='w-3/5 m-auto relative flex flex-col items-center justify-center h-full'>
         <h1 className={`${kimberley.className} text-9xl text-center`}>PROJECT LAIKA</h1>
-        <button className='border-solid border-2 border-white p-3 overflow-hidden
+        <button className={`border-solid border-2 border-white p-3 overflow-hidden
           shadow-white shadow-[6px_6px_0_0_rgb(255,255,255)] text-lg font-bold
-          w-2/4 h-16 mt-20 decoration-2 hover:underline hover:shadow-[10px_10px_0_0_rgb(255,255,255)]
-          transition-all relative' onClick={() => startHandler(true)}>
+          w-2/4 h-16 mt-20 decoration-2 ${!loading ? 'hover:underline hover:shadow-[10px_10px_0_0_rgb(255,255,255)]' : 'pointer-events-none'}
+          transition-all relative`} onClick={() => startHandler(true)}>
           <div className={`${starLoading ? '' : '-translate-x-full'} ease-in-out duration-[2500ms] absolute h-full top-0 left-0 w-full bg-none`}>
             <div className={`${loading ? 'bg-white' : 'bg-none'} ease-in-out duration-[500ms] w-full h-full`} />
           </div>
