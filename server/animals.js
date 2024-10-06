837f4d6404dc69682764b03fb038d13b0db3c570
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-10-06 13:59:00 -0700

    killed index.js

--- server/animals.js
+++ server/animals.js
@@ -1,18 +1,9 @@
-import SECRET from "./secret.json" with { type: "json" };
-
-let api_key = SECRET.api_key
-
-// These need to be retrieved from the user clicking on the map
-let lat = 49
-let lon = -123
 
 // These are hardcoded but will be set permanantly
 const start = 1369728000
 const end = 1369789200
-const radius = 1000 // TODO: this might need to be tweaked
 
-
-async function getCoordinateAnimals(lat, lon, radius) {
+export async function getCoordinateAnimals(lat, lon, radius) {
     const url = `https://api.gbif.org/v1/occurrence/search?decimalLatitude=${lat}&decimalLongitude=${lon}&radius=${radius}&limit=5`;
     try {
         const response = await fetch(url, {
@@ -30,7 +21,6 @@ async function getCoordinateAnimals(lat, lon, radius) {
 
         // filters only animalia results
         const animaliaResults = json.results.filter(result => result.kingdom === 'Animalia');
-        console.log(animaliaResults);
 
         return animaliaResults
     } catch (error) {
@@ -38,4 +28,4 @@ async function getCoordinateAnimals(lat, lon, radius) {
     }
 }
 
-getCoordinateAnimals(lat, lon, radius);
+export default getCoordinateAnimals
--- server/openweather.js
+++ server/openweather.js
@@ -1,7 +1,7 @@
 import SECRET from "./secret.json" with { type: "json" };
 
 // Return Weather Data given coordinates
-async function getCoordinateWeather(lat, lon) {
+export async function getCoordinateWeather(lat, lon) {
     let api_key = SECRET.api_key
 
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`
@@ -20,6 +20,4 @@ async function getCoordinateWeather(lat, lon) {
     }
 }
 
-// let lat = 49.2827
-// let lon = -123.1207
-// getCoordinateWeather(lat, lon)
+export default getCoordinateWeather
